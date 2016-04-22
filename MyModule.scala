f3021f653edaf85c5f4a6b8ae7f6905cbbe7a3bc
ammarm08 <ammar.mian08@gmail.com>
2016-04-22 17:34:14 -0400

    tail recursive solution to fibonacci

--- MyModule.scala
+++ MyModule.scala
@@ -4,12 +4,22 @@ object MyModule {
     else n
 
   def factorial(n: Int): Int = {
-    @annotation.tailrec
     def go(n: Int, acc: Int): Int =
       if (n <= 0) acc
       else go(n - 1, n * acc)
 
     go(n, 1)
+  }
+
+  def fibonacci(n: Int): Int = {
+    @annotation.tailrec
+    def go(current: Int, first: Int, second: Int): Int =
+      if (current >= n) second
+      else go(current + 1, second, first + second)
+
+    if (n <= 1) 0
+    else if (n == 2) 1
+    else go(1, 0, 1)
   }
 
   private def formatResult(name: String, n: Int, f: Int => Int) = {
@@ -20,4 +30,5 @@ object MyModule {
   def main(args: Array[String]): Unit = 
     println(formatResult("absolute value", -42, abs))
     println(formatResult("factorial", 5, factorial))
+    println(formatResult("fibonacci value", 4, fibonacci))
 }
