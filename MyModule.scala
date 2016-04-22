3e91a6016294b04b49866b8db06b837b03e42c58
ammarm08 <ammar.mian08@gmail.com>
2016-04-22 16:33:16 -0400

    refactor formatting into one main func

--- MyModule.scala
+++ MyModule.scala
@@ -4,6 +4,7 @@ object MyModule {
     else n
 
   def factorial(n: Int): Int = {
+    @annotation.tailrec
     def go(n: Int, acc: Int): Int =
       if (n <= 0) acc
       else go(n - 1, n * acc)
@@ -11,16 +12,12 @@ object MyModule {
     go(n, 1)
   }
 
-  private def formatAbs(x: Int) = {
-    val msg = "The absolute value of %d is %d"
-    msg.format(x, abs(x))
-  }
-
-  private def formatFactorial(x: Int) = {
-    val msg = "The factorial of %d is %d"
-    msg.format(x, factorial(x))
+  private def formatResult(name: String, n: Int, f: Int => Int) = {
+    val msg = "The %s of %d is %d"
+    msg.format(name, n, f(n))
   }
 
   def main(args: Array[String]): Unit = 
-    println(formatFactorial(5))
+    println(formatResult("absolute value", -42, abs))
+    println(formatResult("factorial", 5, factorial))
 }
