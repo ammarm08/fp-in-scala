f00b623cfa033e6850efaf770c9b9fbc0f813384
ammarm08 <ammar.mian08@gmail.com>
2016-04-25 12:16:17 -0400

    implement map filter and construct

--- ds.scala
+++ ds.scala
@@ -52,6 +52,42 @@ object DS {
     go(l, Nil)
   }
 
+  def incrementEach(ints: List[Int]): List[Int] = {
+    foldRight(ints, List.empty[Int])((x, acc) => x + 1 :: acc)
+  }
+
+  def stringify(doubles: List[Double]): List[String] = {
+    foldRight(doubles, List.empty[String])((current, acc) => current.toString :: acc)
+  }
+
+  def map[A,B](l: List[A])(f: A => B): List[B] = {
+    foldRight(l, List.empty[B])((current, acc) => f(current) :: acc)
+  }
+
+  def flatMap[A,B](l: List[A])(f: A => List[B]): List[B] = {
+    foldLeft(l, List.empty[B])((acc, current) => acc ++ f(current))
+  }
+
+  def filter[A](l: List[A])(f: A => Boolean): List[A] = {
+    foldRight(l, List.empty[A])((current, acc) => if (f(current)) current :: acc else acc)
+  }
+
+  def flatFilter[A](l: List[A])(f: A => Boolean): List[A] = {
+    flatMap(l)((x) => if (f(x)) List(x) else List.empty[A])
+  }
+
+  def constructList[A](l1: List[A], l2: List[A])(f: (A, A) => A): List[A] = (l1, l2) match {
+    case (Nil, _) => Nil
+    case (_, Nil) => Nil
+    case (xh :: xt, yh :: yt) => (f(xh, yh)) :: constructList(xt, yt)(f)
+  }
+
+  def take[A](l: List[A], n: Int): List[A] = (n, l) match {
+    case (0, _) => Nil
+    case (_, Nil) => Nil
+    case (_, h :: t) => h :: take(t, n - 1)
+  }
+
   // not TCO-ed, will stackoverflow for large lists
   def foldRight[A, B](l: List[A], acc: B)(f: (A, B) => B): B = l match {
     case Nil => acc
