eae0abae4f5480298fe35f47cec733df597671dc
ammarm08 <ammar.mian08@gmail.com>
2016-04-25 18:37:50 -0400

    implement size max depth map methods on tree

--- Tree.scala
+++ Tree.scala
@@ -2,3 +2,25 @@ sealed trait Tree[+A]
 case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
+object Tree {
+
+  def size[A](tree: Tree[A]): Int = tree match {
+    case Leaf(_) => 1
+    case Branch(l, r) => 1 + size(l) + size(r)
+  }
+
+  def maximum(tree: Tree[A]): Int = tree match {
+    case Leaf(n) => n
+    case Branch(l, r) => maximum(l) max maximum(r)
+  }
+
+  def depth(tree: Tree[A]): Int = tree match {
+    case Leaf(_) => 0
+    case Branch(l, r) => 1 + (depth(l) max depth(r))
+  }
+
+  def map[A,B](tree: Tree[A])(f: A => B): Tree[B] = tree match {
+    case Leaf(n) => Leaf(f(n))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
+}
