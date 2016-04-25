18647777874a1c121d8a7ca8eba78c296a184a0f
ammarm08 <ammar.mian08@gmail.com>
2016-04-25 18:53:58 -0400

    implement tree methods with reused fold

--- Tree.scala
+++ Tree.scala
@@ -9,12 +9,12 @@ object Tree {
     case Branch(l, r) => 1 + size(l) + size(r)
   }
 
-  def maximum(tree: Tree[A]): Int = tree match {
+  def maximum(tree: Tree[Int]): Int = tree match {
     case Leaf(n) => n
     case Branch(l, r) => maximum(l) max maximum(r)
   }
 
-  def depth(tree: Tree[A]): Int = tree match {
+  def depth(tree: Tree[Int]): Int = tree match {
     case Leaf(_) => 0
     case Branch(l, r) => 1 + (depth(l) max depth(r))
   }
@@ -22,5 +22,26 @@ object Tree {
   def map[A,B](tree: Tree[A])(f: A => B): Tree[B] = tree match {
     case Leaf(n) => Leaf(f(n))
     case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
+
+  def fold[A,B](tree: Tree[A])(base: A => B)(recur: (B, B) => B): B = tree match {
+    case Leaf(n) => base(n)
+    case Branch(l, r) => recur(fold(l)(base)(recur), fold(r)(base)(recur))
+  }
+
+  def sizeFold[A](tree: Tree[A]): Int = {
+    fold(tree)(x => 1)(1 + _ + _)
+  }
+
+  def maximumFold(tree: Tree[Int]): Int = {
+    fold(tree)(x => x)(_ max _)
+  }
+
+  def depthFold(tree: Tree[Int]): Int = {
+    fold(tree)(x => 0)(1 + (_ max _))
+  }
+
+  def mapFold[A, B](tree: Tree[A])(f: A => B): Tree[B] = {
+    fold(tree)(x => Leaf(f(x)): Tree[B])(Branch(_,_))
   }
 }
