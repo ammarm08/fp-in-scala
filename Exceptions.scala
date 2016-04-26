02c1c091161bcc79254d5d545174e4b6a41ee71f
ammarm08 <ammar.mian08@gmail.com>
2016-04-26 19:07:09 -0400

    sequence with traverse function

--- Exceptions.scala
+++ Exceptions.scala
@@ -107,5 +107,9 @@ def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a match {
   case Nil => Some(Nil)
   case h :: t => map2(f(h), traverse(t)(f))(_ :: _)
 }
+
+// sequence implemented using traverse
+def sequence2[A](a: List[Option[A]]): Option[List[A]] =
+  traverse(a)(x => x)
   
 
