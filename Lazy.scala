3efa81dfda9a1fd66dbd2e3e2e4058aeda1d195e
ammarm08 <ammar.mian08@gmail.com>
2016-04-26 23:24:28 -0400

    use foldRight to build out helper functions

--- Lazy.scala
+++ Lazy.scala
@@ -41,6 +41,49 @@ object Stream {
       lazy val uncons = Some((head, tail))
     }
 
+  // non-strict second param for f (see `=> B`)
+  // if uncons-ing the stream -> Some => return function f w/ params h and recursive fold on tail
+  // if uncons -> None => return function z (base case)
+  // lazy because: only evaluates z if None, and only evaluates f if Some
+  def foldRight[B](z: => B)(f: (A, => B) => B): B = uncons match {
+    case Some((h, t)) => f(h, t.foldRight(z)(f))
+    case None => z
+  }
+
+  // lazy evaluation
+  // if None -> false right away, no other evaluation
+  // else -> predicate check head, or if not valid, check rest of tail
+  def exists(p: A => Boolean): Boolean =
+    foldRight(false)((a, b) => p(a) || b)
+
+  // lazy eval
+  // if None -> false
+  // else predicate check on head AND rest of tail, false if any eval to false
+  def forAll(p: A => Boolean): Boolean =
+    foldRight(true)((a, b) => p(a) && b)
+
+  // lazy eval
+  // if None -> Empty
+  // else predicate check. if true, build stream else empty stream
+  def takeWhile2(f: A => Boolean): Stream[A] =
+    foldRight(empty[A])((h, t) => if (f(h)) cons(h, t) else empty)
+
+  // apply f on h, then cons onto accumulating Stream
+  def map[B](f: A => B): Stream[B] =
+    foldRight(empty[B])((h, t) => cons(f(h), t))
+
+  // only cons h onto stream if it passes predicate else leave acc unchanged
+  def filter(f: A => Boolean): Stream[A] =
+    foldRight(empty[A])((h, t) => if (f(h)) cons(h, t) else t)
+
+  // cons each head 
+  def append[B >: A](s: => Stream[B]): Stream[B] =
+    foldRight(s)((h, t) => cons(h, t))
+
+  // append each streamified head onto tail
+  def flatMap[B](f: A => Stream[B]): Stream[B] =
+    foldRight(empty[B])((h, t) => f(h) append t)
+
   def apply[A](as: A*): Stream[A] =
     if (as.isEmpty) empty
     else cons(as.head, apply(as.tail: _*))
